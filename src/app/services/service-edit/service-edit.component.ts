0a6dda4a471d210b66e85fa04284ae7b7ec5081a
Hyvom <hyvomrahmouni00@gmail.com>
2025-11-18 23:34:11 +0100

    Initial Angular 19 setup with components

---
+++ src/app/services/service-edit/service-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-edit',
+  imports: [],
+  templateUrl: './service-edit.component.html',
+  styleUrl: './service-edit.component.scss'
+})
+export class ServiceEditComponent {
+
+}
