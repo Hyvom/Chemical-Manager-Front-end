551f67ad59d6e0870139a3fc8a08ca3a5e836476
Hyvom <hyvomrahmouni00@gmail.com>
2025-11-22 16:46:53 +0100

    login page styled and updated!

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// login.component.ts
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/auth.service';
@@ -11,15 +12,36 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./login.component.scss'],
   imports: [CommonModule, ReactiveFormsModule, RouterLink]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   error: string | null = null;
   loginForm;
+  currentSlide: number = 0;
+  private slideInterval: any;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  ngOnInit(): void {
+    // Start the automatic slideshow
+    this.startSlideshow();
+  }
+
+  ngOnDestroy(): void {
+    // Clear interval when component is destroyed
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  startSlideshow(): void {
+    // Change slide every 5 seconds
+    this.slideInterval = setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % 4; // 4 slides total
+    }, 5000);
   }
 
   onSubmit() {
